72958345c5f6cf432f6e1592cd5bfc45d2194353
Francisco Javier Cano <fjcano@stratio.com>
2015-04-20 10:11:25 +0200

    Passed first exercice.

--- src/main/scala/com/stratio/model/Flight.scala
+++ src/main/scala/com/stratio/model/Flight.scala
@@ -3,37 +3,42 @@ package com.stratio.model
 import com.stratio.utils.ParserUtils
 import org.joda.time.DateTime
 
-sealed case class Cancelled (id: String) {override def toString: String = id}
+sealed case class Cancelled(id: String) {
 
-object OnTime extends Cancelled (id ="OnTime")
-object Cancel extends Cancelled (id ="Cancel")
-object Unknown extends Cancelled (id ="Unknown")
+  override def toString: String = id
+}
 
-case class Delays (
-    carrier: Cancelled,
-    weather: Cancelled,
-    nAS: Cancelled,
-    security: Cancelled,
-    lateAircraft: Cancelled)
+object OnTime extends Cancelled(id = "OnTime")
 
-case class Flight (date: DateTime, //Tip: Use ParserUtils.getDateTime
-    departureTime: Int,
-    crsDepatureTime: Int,
-    arrTime: Int,
-    cRSArrTime: Int,
-    uniqueCarrier: String,
-    flightNum: Int,
-    actualElapsedTime: Int,
-    cRSElapsedTime: Int,
-    arrDelay: Int,
-    depDelay: Int,
-    origin: String,
-    dest: String,
-    distance: Int,
-    cancelled: Cancelled,
-    cancellationCode: Int,
-    delay: Delays)
-{
+object Cancel extends Cancelled(id = "Cancel")
+
+object Unknown extends Cancelled(id = "Unknown")
+
+case class Delays(
+                   carrier: Cancelled,
+                   weather: Cancelled,
+                   nAS: Cancelled,
+                   security: Cancelled,
+                   lateAircraft: Cancelled)
+
+case class Flight(date: DateTime, //Tip: Use ParserUtils.getDateTime
+                  departureTime: Int,
+                  crsDepatureTime: Int,
+                  arrTime: Int,
+                  cRSArrTime: Int,
+                  uniqueCarrier: String,
+                  flightNum: Int,
+                  actualElapsedTime: Int,
+                  cRSElapsedTime: Int,
+                  arrDelay: Int,
+                  depDelay: Int,
+                  origin: String,
+                  dest: String,
+                  distance: Int,
+                  cancelled: Cancelled,
+                  cancellationCode: Int,
+                  delay: Delays) {
+
   def isGhost: Boolean = arrTime == -1
 
   def departureDate: DateTime =
@@ -46,14 +51,34 @@ case class Flight (date: DateTime, //Tip: Use ParserUtils.getDateTime
       .plusMinutes(cRSElapsedTime)
 }
 
-object Flight{
+object Flight {
 
   /*
   *
   * Create a new Flight Class from a CSV file
   *
   */
-  def apply(fields: Array[String]): Flight = ???
+  def apply(fields: Array[String]): Flight = {
+    new Flight(ParserUtils.getDateTime(fields(0).toInt, fields(1).toInt, fields(2).toInt),
+               fields(4).toInt,
+               fields(5).toInt,
+               fields(6).toInt,
+               fields(7).toInt,
+               fields(8),
+               fields(9).toInt,
+               fields(11).toInt,
+               fields(12).toInt,
+               fields(14).toInt,
+               fields(15).toInt,
+               fields(16),
+               fields(17),
+               fields(18).toInt,
+               parseCancelled(fields(21)),
+               fields(22).toInt,
+               Delays(parseCancelled(fields(24)), parseCancelled(fields(25)), parseCancelled(fields(26)),
+                      parseCancelled(fields(27)),
+                      parseCancelled(fields(28))))
+  }
 
   /*
    *
@@ -69,5 +94,11 @@ object Flight{
   *   if field == 0 -> OnTime
   *   if field <> 0 && field<>1 -> Unknown
   */
-  def parseCancelled(field: String): Cancelled = ???
+  def parseCancelled(field: String): Cancelled = {
+    field match {
+      case "1" => Cancel
+      case "0" => OnTime
+      case _ => Unknown
+    }
+  }
 }
--- src/main/scala/com/stratio/model/spark/FlightDsl.scala
+++ src/main/scala/com/stratio/model/spark/FlightDsl.scala
@@ -6,74 +6,76 @@ import org.apache.spark.rdd.RDD
 
 class FlightCsvReader(self: RDD[String]) {
 
-    /**
-     *
-     * Parser the csv file with the format described in te readme.md file to a Fligth class
-     *
-     */
-    def toFlight: RDD[Flight] = ???
-
-    /**
-     *
-     * Obtain the parser errors
-     *
-     */
-    def toErrors: RDD[(String, String)] = ???
+  /**
+   *
+   * Parser the csv file with the format described in te readme.md file to a Fligth class
+   *
+   */
+  def toFlight: RDD[Flight] = {
+    self.map { l => Flight(l.split(","))}
   }
 
-  class FlightFunctions(self: RDD[Flight]) {
+  /**
+   *
+   * Obtain the parser errors
+   *
+   */
+  def toErrors: RDD[(String, String)] = ???
+}
 
-    /**
-     *
-     * Obtain the minimum fuel's consumption using a external RDD with the fuel price by Year, Month
-     *
-     */
-    def minFuelConsumptionByMonthAndAirport(fuelPrice: RDD[String]): RDD[(String, Short)] = ???
-
-    /**
-     *
-     * Obtain the average distance fliyed by airport, taking the origin field as the airport to group
-     *
-     */
-    def averageDistanceByAirport: RDD[(String, Float)] = ???
+class FlightFunctions(self: RDD[Flight]) {
 
   /**
-    * A Ghost Flight is each flight that has arrTime = -1 (that means that the flight didn't land where was supposed to
-    * do it).
-    *
-    * The correct assign to those flights will be :
-    *
-    * The ghost's flight arrTime would take the data of the nearest flight in time that has its flightNumber and which
-    * deptTime is lesser than the ghost's flight deptTime plus a configurable amount of seconds, from here we will call
-    * this flight: the saneated Flight
-    *
-    * So if there is no sanitized Flight for a ghost Flight we will return the same ghost Flight but if there are some
-    * sanitized Flight we had to assign the following values to the ghost flight:
-    *
-    * dest = sanitized Flight origin
-    * arrTime = sanitized Flight depTime
-    * csrArrTime = sanitized Flight csrDepTime
-    * date =  sanitized Flight date
-    *
-    * --Example
-    *   window time = 600 (10 minutes)
-    *   flights before resolving ghostsFlights:
-    *   flight1 = flightNumber=1, departureTime=1-1-2015 deparHour810 arrTime=-1 orig=A dest=D
-    *   flight2 = flightNumber=1, departureTime=1-1-2015 departureTime=819 arrTime=1000 orig=C dest=D
-    *   flight3 = flightNumber=1,  departureTime=1-1-2015 departureTime=815 arrTime=816 orig=B dest=C
-    *   flight4 = flightNumber=2, departureTime=1-1-2015 deparHour810 arrTime=-1 orig=A dest=D
-    *   flight5 = flightNumber=2, departureTime=1-1-2015 deparHour821 arrTime=855 orig=A dest=D
-    *  flights after resolving ghostsFlights:
-    *   flight1 = flightNumber=1, departureTime=1-1-2015 deparHour810 arrTime=815 orig=A dest=B
-    *   flight2 = flightNumber=1, departureTime=1-1-2015 departureTime=819 arrTime=1000 orig=C dest=D
-    *   flight3 = flightNumber=1,  departureTime=1-1-2015 departureTime=815 arrTime=816 orig=B dest=C
-    *   flight4 = flightNumber=2, departureTime=1-1-2015 deparHour810 arrTime=-1 orig=A dest=D
-    *   flight5 = flightNumber=2, departureTime=1-1-2015 deparHour821 arrTime=855 orig=A dest=D
-    */
+   *
+   * Obtain the minimum fuel's consumption using a external RDD with the fuel price by Year, Month
+   *
+   */
+  def minFuelConsumptionByMonthAndAirport(fuelPrice: RDD[String]): RDD[(String, Short)] = ???
+
+  /**
+   *
+   * Obtain the average distance fliyed by airport, taking the origin field as the airport to group
+   *
+   */
+  def averageDistanceByAirport: RDD[(String, Float)] = ???
+
+  /**
+   * A Ghost Flight is each flight that has arrTime = -1 (that means that the flight didn't land where was supposed to
+   * do it).
+   *
+   * The correct assign to those flights will be :
+   *
+   * The ghost's flight arrTime would take the data of the nearest flight in time that has its flightNumber and which
+   * deptTime is lesser than the ghost's flight deptTime plus a configurable amount of seconds, from here we will call
+   * this flight: the saneated Flight
+   *
+   * So if there is no sanitized Flight for a ghost Flight we will return the same ghost Flight but if there are some
+   * sanitized Flight we had to assign the following values to the ghost flight:
+   *
+   * dest = sanitized Flight origin
+   * arrTime = sanitized Flight depTime
+   * csrArrTime = sanitized Flight csrDepTime
+   * date =  sanitized Flight date
+   *
+   * --Example
+   * window time = 600 (10 minutes)
+   * flights before resolving ghostsFlights:
+   * flight1 = flightNumber=1, departureTime=1-1-2015 deparHour810 arrTime=-1 orig=A dest=D
+   * flight2 = flightNumber=1, departureTime=1-1-2015 departureTime=819 arrTime=1000 orig=C dest=D
+   * flight3 = flightNumber=1,  departureTime=1-1-2015 departureTime=815 arrTime=816 orig=B dest=C
+   * flight4 = flightNumber=2, departureTime=1-1-2015 deparHour810 arrTime=-1 orig=A dest=D
+   * flight5 = flightNumber=2, departureTime=1-1-2015 deparHour821 arrTime=855 orig=A dest=D
+   * flights after resolving ghostsFlights:
+   * flight1 = flightNumber=1, departureTime=1-1-2015 deparHour810 arrTime=815 orig=A dest=B
+   * flight2 = flightNumber=1, departureTime=1-1-2015 departureTime=819 arrTime=1000 orig=C dest=D
+   * flight3 = flightNumber=1,  departureTime=1-1-2015 departureTime=815 arrTime=816 orig=B dest=C
+   * flight4 = flightNumber=2, departureTime=1-1-2015 deparHour810 arrTime=-1 orig=A dest=D
+   * flight5 = flightNumber=2, departureTime=1-1-2015 deparHour821 arrTime=855 orig=A dest=D
+   */
 
   def asignGhostFlights(elapsedSeconds: Int): RDD[Flight] = ???
 
-  }
+}
 
 
 trait FlightDsl {
